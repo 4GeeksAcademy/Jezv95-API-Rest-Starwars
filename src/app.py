cf8fd0c8d4ef16922546fb33c312f6ca8ce4ad58
Jezv95 <138320134+Jezv95@users.noreply.github.com>
2025-08-26 23:05:14 +0000

    created GET,POST and Delete

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,People,Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,127 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+ # Users Route-------------
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    all_users = User.query.all()
+    results = map(lambda user: user.serialize() ,all_users)
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "these are all the users:",
+        "users": list(results),
+        "planets_fav":[],
+        "people_fav":[],
+
     }
 
     return jsonify(response_body), 200
+
+
+
+   # People Route------------- 
+
+@app.route('/people', methods=['GET'])
+def handle_people():
+    all_people = People.query.all()
+    people_results = map(lambda people: people.serialize() ,all_people)
+
+    people_response_body = {
+        "msg": "these are all the people:",
+        "people": list(people_results),
+    }
+    return jsonify(people_response_body), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_person(people_id):
+    person = People.query.filter_by(id = people_id).first()
+  
+
+    return jsonify(person.serialize()), 200
+
+
+@app.route('/people', methods=['POST'])
+def add_people():
+
+    person_body =request.get_json()
+        
+   
+  
+    person = People( 
+        name=person_body['name'],
+        eye_color=person_body['eye_color'],
+        gender=person_body['gender'],
+        hair_color=person_body['hair_color'],
+        height=person_body['height'],)
+    db.session.add(person)
+    db.session.commit()
+    
+    addchar_response_body = {
+        "msg": "a new character added succesfully",
+        "character" : person.serialize()
+        
+    }
+    return jsonify(addchar_response_body), 200
+
+@app.route('/people/<int:people_id>', methods=['DELETE'])
+def delete_person(people_id):
+    person = People.query.filter_by(id = people_id).first()
+  
+    db.session.delete(person)
+    db.session.commit()
+    return jsonify({
+        "msg": " Deleted succesfully",
+        "character" : person.serialize()
+        
+    }), 200
+
+
+
+# Planets Route------------- 
+
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+    all_planets = Planet.query.all()
+    planets_results = map(lambda planet : planet.serialize() ,all_planets)
+
+    planets_response_body = {
+        "msg": "these are all the people:",
+        "planets": list(planets_results),
+    }
+    return jsonify(planets_response_body), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def handle_planet(planet_id):
+    planet = Planet.query.filter_by(id = planet_id).first()
+  
+
+    return jsonify(planet.serialize()), 200
+
+
+@app.route('/planets', methods=['POST'])
+def add_planets():
+
+    planet_body=request.get_json()
+    planet = Planet( 
+        name=planet_body['name'],
+        diameter=planet_body['diameter'],
+        gravity=planet_body['gravity'],
+        population=planet_body['population'],
+        climate=planet_body['climate'],)
+    db.session.add(planet)
+    db.session.commit()
+
+
+    
+    addplanet_response_body = {
+        "msg": "a new planet added succesfully",
+        
+    }
+    return jsonify(addplanet_response_body), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,14 +6,57 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), nullable=False)
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
 
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class Planet (db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    diameter: Mapped[str] = mapped_column(String(120), nullable=False)
+    gravity: Mapped[str] = mapped_column(String(120), nullable=False)
+    population: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+        }
+
+class People (db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(120), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    height: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "height": self.height,
         }
